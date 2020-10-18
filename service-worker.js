47394de823da79b0c4a91fcaf0dbfaefa1afcca7
DoryaDev <73027480+DoryaDev@users.noreply.github.com>
2020-10-18 18:03:34 +0200

    Change

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.59bb59b168884792f1c237b496c016bc.js"
+  "/precache-manifest.33dac5fa0def11469f0f7673f10b7bab.js"
 );
 
 self.addEventListener('message', (event) => {
